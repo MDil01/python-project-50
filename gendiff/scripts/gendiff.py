3e38fa6b2d4ecfe5589626aaa738fd34853d38eb
Dil <m.dilyara2010@yandex.ru>
2024-03-06 08:44:44 +0300

    add command gendiff -h

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+from gendiff import cli
+# from gendiff import generate_diff
+
+
+def main():
+    '''
+    This function is the entry point of the program.
+    It retrieves file paths and format name
+    from the command line interface, generates the difference
+    between the files, and prints the difference.
+    '''
+    file_path1, file_path2, format_name = cli()
+    # diff = generate_diff(file_path1, file_path2, format_name)
+    # print(diff)
+
+
+if __name__ == "__main__":
+    main()
